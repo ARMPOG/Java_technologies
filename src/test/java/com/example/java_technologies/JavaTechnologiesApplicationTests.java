f41ee2a592a1783165d3ee82fcc8997ba59bdec1
Armen Poghosyan <armpog@gmail.com>
2020-03-26 14:12:59 +0400

    first commit

---
+++ src/test/java/com/example/java_technologies/JavaTechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_technologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechnologiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
